b734da2a1e7663499c7b80c271276c8ca9f5b8de
CONCERTO_L208 <CONCERTO_L208@CONCERTO-L208>
2022-10-09 17:46:57 +0530

    First commit

---
+++ Suvidha_Bank/src/main/java/com/Suvidha_Bank/entity/Collateral.java
@@ -0,0 +1,29 @@
+package com.Suvidha_Bank.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+@Entity
+public class Collateral {
+	@Id
+	private String collateralId;
+	private String collateralType;
+	private String loanId;
+	
+	
+}
